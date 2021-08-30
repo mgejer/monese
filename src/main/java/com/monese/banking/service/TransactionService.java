7762a7e1849b3fe6d96c28c46158b1cefecbd6a8
Martin Gejer <mgejer123@gmail.com>
2021-08-30 22:02:54 +0100

    find transactions by account in service

--- src/main/java/com/monese/banking/service/TransactionService.java
+++ src/main/java/com/monese/banking/service/TransactionService.java
@@ -3,14 +3,25 @@ package com.monese.banking.service;
 import com.monese.banking.dao.TransactionRepository;
 import com.monese.banking.model.Status;
 import com.monese.banking.model.Transaction;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
 
 public class TransactionService {
 
     private TransactionRepository repository;
 
+    @Value("${default.transactions.by.account.max.amount.days.search: 30}")
+    private Integer defaultAmountOfDaysSearch;
+
+    public Collection<Transaction> findByAccount(long accountId, Optional<Integer> amountOfDays) {
+        return repository.findByAccountId(accountId, now().minusDays(amountOfDays.orElse(defaultAmountOfDaysSearch)));
+    }
+
     public Transaction createSuccessfulTransaction (double amount, long from, long to) {
-        return repository.save(new Transaction(amount, LocalDateTime.now(), from, to, Status.SUCCESSFUL));
+        return repository.save(new Transaction(amount, now(), from, to, Status.SUCCESSFUL));
     }
 }
